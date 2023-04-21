33c48bfead4f62b80aad4ad25900236b961c2e80
Lucas Bezerra <1lcs.bzrr@gmail.com>
2023-04-21 19:38:05 -0300

    reverse para o codigo antigo

--- index.js
+++ index.js
@@ -1,131 +1,86 @@
+require("dotenv").config();
 const express = require("express");
-const venom = require("venom-bot");
-const socketIO = require("socket.io");
-const http = require("http");
-const cors = require("cors");
-const fs = require("fs");
-
-const tokensPath = "./tokens";
-
-if (!fs.existsSync(tokensPath)) {
-  fs.mkdirSync(tokensPath);
-}
-
-const tokenFolder = fs.readdirSync(tokensPath);
+const { Client } = require("whatsapp-web.js");
+const qrcodeTerminal = require("qrcode-terminal");
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: "*",
-  },
-});
+var cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
-const venomInstances = {};
+const clients = {};
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-function sendMessage(client, number, message, res) {
-  client
-    .sendText(`${number}@c.us`, message)
-    .then(() => res.sendStatus(200))
-    .catch(() => res.status(500).send("Error sending message"));
+function removeNonAlphaNumeric(str) {
+  return str.replace(/[^a-zA-Z0-9]/g, "");
 }
 
-async function createVenomInstance(id) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const client = await venom.create(
-        id,
-        (base64Qrimg) => {
-          io.emit("qrCode", { id, qrCode: base64Qrimg });
-        },
-        (status) => {
-          venomInstances[id] = { ...venomInstances[id], status: status };
-
-          if (venomInstances[id]) {
-            io.emit("status", { id, status });
-          }
-        },
-        {
-          logQR: false,
-          autoClose: false,
-          disableWelcome: true,
-        }
-      );
-
-      if (!venomInstances[id]) {
-        venomInstances[id] = {};
-      }
-
-      venomInstances[id].client = client;
-
-      resolve(client);
-    } catch (error) {
-      console.error(error);
-      reject(error);
+app.post("/create", (req, res) => {
+  const id = removeNonAlphaNumeric(req.body.id);
+  if (clients[id]) {
+    if (clients[id].qrcode) {
+      res.json({ qrcode: clients[id].qrcode }).status(200).end();
     }
-  });
-}
+  } else {
+    const client = new Client({
+      puppeteer: {
+        args: ["--no-sandbox"],
+      },
+    });
 
-app.post("/check-status", async (req, res) => {
-  const { id } = req.body;
+    console.log(`\n Iniciando o cliente: "${id}"`);
 
-  if (!venomInstances[id]) {
-    res.status(200).json({ status: "notFound" }).end();
-    return;
+    clients[id] = client;
+    clients[id].isReady = false;
+
+    clients[id].on("qr", (qrcode) => {
+      console.log(`\nQRCode recebido com sucesso para o cliente: "${id}"`);
+
+      qrcodeTerminal.generate(qrcode, { small: true });
+
+      clients[id].qrcode = qrcode;
+
+      res.json({ qrcode: qrcode }).status(200).end();
+    });
+
+    clients[id].on("ready", () => {
+      clients[id].isReady = true;
+      res.status(200).end();
+    });
+
+    clients[id].initialize();
   }
-
-  res.status(200).json({ status: venomInstances[id].status }).end();
-});
-
-app.post("/create", async (req, res) => {
-  const { id } = req.body;
-
-  if (venomInstances[id] && tokenFolder.includes(id)) {
-    res.status(202).json({ status: venomInstances[id].status }).end();
-    return;
-  }
-
-  await createVenomInstance(id);
 });
 
 app.post("/send-message", async (req, res) => {
-  const { id, number, message } = req.body;
-  const client = venomInstances[id].client;
+  const id = removeNonAlphaNumeric(req.body.id);
 
-  if (!client) {
-    await createVenomInstance(id, res);
-    sendMessage(venomInstances[id], number, message, res);
-  } else {
-    sendMessage(client, number, message, res);
+  if (!req.body.number) {
+    res.emit("error").status(404).end();
   }
-});
 
-app.post("/disconnect", async (req, res) => {
-  const { id } = req.body;
-  const client = venomInstances[id];
+  const number = removeNonAlphaNumeric(req.body.number);
+  const message = req.body.message;
 
-  if (!client) {
-    res.status(400).send("Instance not found");
+  const chatId = number + "@c.us";
+
+  if (!clients[id] || clients[id].isReady === false) {
+    res.status(404).end();
     return;
   }
 
-  client
-    .close()
-    .then(() => {
-      delete venomInstances[id];
-      res.status(200).send("Instance disconnected");
-    })
-    .catch((error) => {
-      res.status(500).send("Error disconnecting instance");
-    });
+  try {
+    await clients[id].sendMessage(chatId, message);
+    res.status(200).end();
+  } catch (err) {
+    console.error(err);
+    res.emit("error").status(404).end();
+  }
 });
 
-const PORT = process.env.PORT || 3535;
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Servidor iniciado");
+  console.log("Porta: " + PORT);
+});
